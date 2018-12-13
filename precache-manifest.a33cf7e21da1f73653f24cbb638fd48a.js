01bd7a3aadfe8ef4524ac18e2c602c3dd870abd6
Jeffrey Van Rees <jvanrees@wisc.edu>
2018-12-12 19:19:06 -0700

    test for new portfolio

---
+++ precache-manifest.a33cf7e21da1f73653f24cbb638fd48a.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a2061a11f8b261a78cf4bd9b3d077d7",
+    "url": "/static/media/desktop_screenshot.0a2061a1.png"
+  },
+  {
+    "revision": "9e4dade4ffae9f2171c7",
+    "url": "/static/css/main.ae93cf12.chunk.css"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "d7157adacea58e3a3245",
+    "url": "/static/js/1.d7157ada.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "01b746096b046380dd18272ad96ffc12",
+    "url": "/static/media/Screenshot_20181130-023138.01b74609.png"
+  },
+  {
+    "revision": "80192878c80ddd5f7f106d019e02a03d",
+    "url": "/static/media/Screenshot_20181130-023540.80192878.png"
+  },
+  {
+    "revision": "239e21275bd52b0b2d4c09948df8f361",
+    "url": "/static/media/Screenshot_20181130-023613.239e2127.png"
+  },
+  {
+    "revision": "a0bb404c551e79ef3abe6749b5c6f6df",
+    "url": "/static/media/Screenshot_20181130-023746.a0bb404c.png"
+  },
+  {
+    "revision": "b808a0747ab3a0d7a9074ce47e119ca8",
+    "url": "/static/media/logo.b808a074.svg"
+  },
+  {
+    "revision": "d512251058ff6f82b1709302d9448e47",
+    "url": "/static/media/mail_icon.d5122510.svg"
+  },
+  {
+    "revision": "31352e107131818807a09bcaad6b5141",
+    "url": "/static/media/linked_in.31352e10.svg"
+  },
+  {
+    "revision": "06ea5607d96b56c2c9617e58818cb8d3",
+    "url": "/static/media/logo_row.06ea5607.svg"
+  },
+  {
+    "revision": "113a0b1fe6abf9e1b2036d915922d341",
+    "url": "/static/media/headshot_b.113a0b1f.jpg"
+  },
+  {
+    "revision": "27d619c36ad931009a5d18613eadfb81",
+    "url": "/static/media/Screenshot_20181103-203245_Rocky Mountain Arsenal History.27d619c3.jpg"
+  },
+  {
+    "revision": "71ad004440fc04a497098a905d2b252a",
+    "url": "/static/media/Screenshot_20181103-203534_Rocky Mountain Arsenal History.71ad0044.jpg"
+  },
+  {
+    "revision": "b664622000c7701100d4ec8d71ea76ff",
+    "url": "/static/media/Screenshot_20181103-203554_Rocky Mountain Arsenal History.b6646220.jpg"
+  },
+  {
+    "revision": "62098a523a0b7be3891f69b7848a5f07",
+    "url": "/static/media/Screenshot_20181103-204001_Rocky Mountain Arsenal History.62098a52.jpg"
+  },
+  {
+    "revision": "5550d4d5bb616de8bbdbc0d17ec0dca5",
+    "url": "/static/media/Screenshot_20181103-204113_Rocky Mountain Arsenal History.5550d4d5.jpg"
+  },
+  {
+    "revision": "9e4dade4ffae9f2171c7",
+    "url": "/static/js/main.9e4dade4.chunk.js"
+  },
+  {
+    "revision": "0e760e811fe8ab086d7b044f77201eaf",
+    "url": "/static/media/disastermng_db.0e760e81.png"
+  },
+  {
+    "revision": "2e26d4d7e383e4d9dfb41a29b558128c",
+    "url": "/static/media/ihateeclipse.2e26d4d7.png"
+  },
+  {
+    "revision": "9dee9ae55b52e3b8574842b09b882040",
+    "url": "/static/media/Screenshot_20181130-023757.9dee9ae5.png"
+  },
+  {
+    "revision": "c66da357191a4fe2944657a76ba54d29",
+    "url": "/static/media/Screenshot_20181130-023810.c66da357.png"
+  },
+  {
+    "revision": "c82dbc3ef360249fa681575099413e65",
+    "url": "/static/media/Screenshot_20181130-023817.c82dbc3e.png"
+  },
+  {
+    "revision": "1e95e2b0a7850891b845d0677cf49b42",
+    "url": "/static/media/Screenshot_20181130-023825.1e95e2b0.png"
+  },
+  {
+    "revision": "2645a132677b605635039e1e33d08bbc",
+    "url": "/static/media/concrete-wall-3.2645a132.png"
+  },
+  {
+    "revision": "27de9687c3a592a3e404f23f17af61a4",
+    "url": "/static/media/google_maps_android.27de9687.jpg"
+  },
+  {
+    "revision": "53100016e670f2c270f349167267bd17",
+    "url": "/static/media/leaflet_map.53100016.jpg"
+  },
+  {
+    "revision": "50f9e58faf2d8a0ccd07916a46e54d03",
+    "url": "/static/media/shaded_relief.50f9e58f.jpg"
+  },
+  {
+    "revision": "7c952fd1662de47db9ba21729406823b",
+    "url": "/static/media/new_night_mode.7c952fd1.png"
+  },
+  {
+    "revision": "8dba5c892dd7d8d01b48911ddfb356c1",
+    "url": "/static/media/ming_cartocss.8dba5c89.png"
+  },
+  {
+    "revision": "2e7fb00d30e0c3cfeb58a56b4c1698c1",
+    "url": "/static/media/rma_node_express.2e7fb00d.png"
+  },
+  {
+    "revision": "0471cca8f9ca2d80830d3a0c24dab056",
+    "url": "/static/media/rma_history_android.0471cca8.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d7157adacea58e3a3245",
+    "url": "/static/css/1.4902bf20.chunk.css"
+  },
+  {
+    "revision": "c302b430b35ab89f3477b6e083b3e89a",
+    "url": "/index.html"
+  }
+];
