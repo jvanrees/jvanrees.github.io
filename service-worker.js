710cf0183a115c65a40c1e1720a7aabb9d506b6b
Jeff Van Rees <jvanrees@wisc.edu>
2019-01-25 14:18:28 -0700

    Images resized, email updated

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4339a737cd07486305933a3b7a4a372c.js"
+  "/precache-manifest.ce24bbb3536b1110deddddf32ecfbce5.js"
 );
 
 workbox.clientsClaim();
